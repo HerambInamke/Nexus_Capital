b339e9a0655371259b431fe2db6d02e19666fcb0
HerambInamke <heramb15012006@gmail.com>
2025-03-01 22:27:24 +0530

    delete_sab

--- moneymate-client/src/app/models/savings.model.ts
+++
@@ -1,9 +0,0 @@
-export interface SavingsGoal {
-  _id: string;
-  name: string;
-  targetAmount: number;
-  currentAmount: number;
-  targetDate: Date;
-  description: string;
-  priority: 'low' | 'medium' | 'high';
-}
--- moneymate-client/src/app/models/transaction.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Transaction {
-    _id: string;
-    category: string;
-    amount: number;
-    date: Date;
-    description: string;
-  }
